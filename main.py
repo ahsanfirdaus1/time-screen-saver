814befc3300753425c924157f212cc96d545d111
ahsanfirdaus1 <ahsanfirdaus1@gmail.com>
2021-09-09 21:50:15 +0700

    Memasaang Label

--- main.py
+++ main.py
@@ -8,4 +8,7 @@ Screen.attributes("-fullscreen", True)
 Screen.bind("<F11>", lambda event: Screen.attributes("-fullscreen", not Screen.attributes("-fullscreen")))
 Screen.bind("<Escape>", lambda event: Screen.attributes("-fullscreen", False))
 
+labelWaktu = Label(Screen, text = "", font= ("Kameron", 60), fg = "#03fc35", bg = "black" )
+labelWaktu.pack(pady= 250)
+
 Screen.mainloop()
