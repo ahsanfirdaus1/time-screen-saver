52d37e7552c73d9a293ac93fed7c1c5a2f02e39d
ahsanfirdaus1 <ahsanfirdaus1@gmail.com>
2021-09-09 21:43:48 +0700

    Konfigurasi supaay full screen dan bisa keluar dari fullscreen dengan menekan F11

--- main.py
+++ main.py
@@ -4,3 +4,8 @@ from typing import Sized
 
 Screen = Tk()
 
+Screen.attributes("-fullscreen", True)
+Screen.bind("<F11>", lambda event: Screen.attributes("-fullscreen", not Screen.attributes("-fullscreen")))
+Screen.bind("<Escape>", lambda event: Screen.attributes("-fullscreen", False))
+
+Screen.mainloop()
